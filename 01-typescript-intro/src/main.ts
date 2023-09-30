c2e319bc67e81e363e3a670507b61b78a89dc3e3
Diego Santacruz <dsantacruz@gmail.com>
2023-09-30 20:12:59 -0300

    destructuring

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -5,8 +5,9 @@ import { setupCounter } from './counter.ts'
 
 //import './topics/01-basic-types.ts';
 //import './topics/02-object-interface.ts';
-import './topics/03-functions.ts'
-
+//import './topics/03-functions.ts';
+//import './topics/04-homework-types.ts';
+import './topics/05-basisc-destructuring.ts';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   hola Mundo!
