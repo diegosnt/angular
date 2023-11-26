0ee947e787ed0ab957bd07b6372ae7dd4cde01f9
Diego Santacruz <dsantacruz@gmail.com>
2023-11-26 01:05:53 -0300

    extender de una clase

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,7 +1,7 @@
 import './style.css'
 // import typescriptLogo from './typescript.svg'
 // import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+//import { setupCounter } from './counter.ts'
 
 //import './topics/01-basic-types.ts';
 //import './topics/02-object-interface.ts';
@@ -9,10 +9,10 @@ import { setupCounter } from './counter.ts'
 //import './topics/04-homework-types.ts';
 //import './topics/05-basisc-destructuring.ts';
 //import './topics/06-function-destructuring.ts'
-import './topics/07-import-export.ts'
+//import './topics/07-import-export.ts'
+import './topics/08-clases.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  hola Mundo!
-`
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `hola Mundo!`
+
 console.log('Hola Mundo');
 
