3076f3d3d877113cc953cb9e5fdbf960dbe76fe0
Diego Santacruz <dsantacruz@gmail.com>
2024-08-04 09:56:31 -0300

    se crean los componentes de heroes

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -6,13 +6,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title:string = 'Mi primer app de Angular';
-  public counter: number = 10;
-
-  increaseBy(value: number):void{
-    this.counter += value;
-  }
-
-  reset(value: number):void{
-    this.counter = value;
-  }
 }
